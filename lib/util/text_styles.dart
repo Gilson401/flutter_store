25b42c7ba8b275e7e0042b648da3d43ec879c88e
Gilson Santos <gilson.santos@infobase.com.br>
2024-05-26 15:21:27 -0300

    fontes

---
+++ lib/util/text_styles.dart
@@ -0,0 +1,201 @@
+import 'colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  const AppTextStyles();
+
+  static TextStyle get appBarTitle => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: appBarTitleColor,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get appBarSubtitle => const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: appBarSubtitleColor,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get text => const TextStyle(
+      fontSize: 16, fontWeight: FontWeight.w400, color: whiteFlutterStore);
+
+  static TextStyle get blackText => const TextStyle(
+      fontSize: 16, fontWeight: FontWeight.w400, color: primaryColor);
+
+  static TextStyle get blackTextBold => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: primaryColor,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get linkText => const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: whiteFlutterStore,
+      decoration: TextDecoration.underline,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s16w400cWhite => const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s16w500White => const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s20w700cWhite => const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s20w500cWhite => const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s12w700cWhite => const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s16w700cWhite => const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s18w400cWhite => const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s18w600cWhite => const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s12w600cWhite => const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w600,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s12w400cWhite => const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s14w500cWhite => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s14w600cWhite => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s14w400cWhite => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s24w700cWhite => const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s46w700cWhite => const TextStyle(
+      fontSize: 46,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+
+  static TextStyle get s22w800cWhite => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w800,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s14w500cYellow => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: yellowFootstas,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s14w700cYellow => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: yellowFootstas,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s13w700cWhite => const TextStyle(
+      fontSize: 13,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s14w700cWhite => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s10w400cWhite => const TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      color: Colors.white,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s14w700cGray => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: grayFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s12w700cGray => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: grayFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s12w500cWhite => const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s12w400cGray => const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: grayFlutterStore,
+      fontFamily: 'Montserrat');
+
+  static TextStyle get s31w700cWhite => const TextStyle(
+      fontSize: 31,
+      fontWeight: FontWeight.w700,
+      color: whiteFlutterStore,
+      fontFamily: 'Montserrat');
+
+    static TextStyle get s40w500cGray => const TextStyle(
+      fontSize: 40,
+      fontWeight: FontWeight.w500,
+      color: cardBackground,
+      fontFamily: 'Inter');    
+
+}
