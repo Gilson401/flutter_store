3f970d13273b4d234032858b9cb90bb17b217e92
Gilson Santos <gilson.santos@infobase.com.br>
2024-05-26 15:20:08 -0300

    remove unused

--- lib/app/controller/home_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_store/app/controller/click_model.dart';
-import 'package:get/get.dart';
-
-class HomeController extends GetxController {
-  final click = Get.put(Click(tap: 10));
-}
